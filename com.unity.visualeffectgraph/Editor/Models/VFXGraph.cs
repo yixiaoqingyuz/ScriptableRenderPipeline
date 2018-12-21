c2f93c7467bee98adfe4753bbd119e6cf2655a33
Tristan Genevet <tristan@unity3d.com>
2018-12-21 19:22:09 +0100

    trying to get graph to update when subgraph values are changed.

--- com.unity.visualeffectgraph/Editor/Models/VFXGraph.cs
+++ com.unity.visualeffectgraph/Editor/Models/VFXGraph.cs
@@ -501,14 +501,16 @@ namespace UnityEditor.VFX
         {
             if (m_SubgraphDependencies != null && m_SubgraphDependencies.Contains(subgraph))
             {
-                RecurseSubgraphRecreateCopy(this);
                 if (expressionsChanged)
                 {
+                    RecurseSubgraphRecreateCopy(this);
                     compiledData.Compile(m_CompilationMode, m_ForceShaderValidation);
                     m_ExpressionGraphDirty = false;
                 }
                 else
+                {
                     compiledData.UpdateValues();
+                }
 
                 m_ExpressionValuesDirty = false;
             }
@@ -566,8 +568,9 @@ namespace UnityEditor.VFX
                     }
                     m_ExpressionGraphDirty = false;
 
-                    m_ExpressionValuesDirty = false; //TODOtristan make the code that updates the value.
+                    m_ExpressionValuesDirty = false;
                 }
+                   
             }
         }
 

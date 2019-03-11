222e630e3256853192d73f983a482cac4dc7e07a
RSlysz <remi@unity3d.com>
2019-03-11 18:21:58 +0100

    Add first step on window layouts in UIElement

--- com.unity.render-pipelines.core/Editor/LookDev/LookDevStyle.cs
+++ com.unity.render-pipelines.core/Editor/LookDev/LookDevStyle.cs
@@ -6,7 +6,8 @@ namespace UnityEditor.Rendering.LookDev
     internal class LookDevStyle
     {
         const string k_IconFolder = @"Packages/com.unity.render-pipelines.core/Editor/LookDev/Icons/";
-        
+        internal const string k_uss = @"Packages/com.unity.render-pipelines.core/Editor/LookDev/LookDevWindow.uss";
+
         public static readonly GUIContent WindowTitleAndIcon = EditorGUIUtility.TrTextContentWithIcon("Look Dev", CoreEditorUtils.LoadIcon(k_IconFolder, "LookDevMainIcon"));
     }
 }
--- com.unity.render-pipelines.core/Editor/LookDev/LookDevWindow.cs
+++ com.unity.render-pipelines.core/Editor/LookDev/LookDevWindow.cs
@@ -1,16 +1,77 @@
+using UnityEditor.UIElements;
+using UnityEngine.UIElements;
+
 namespace UnityEditor.Rendering.LookDev
 {
+
+    public enum Layout { ViewA, ViewB, HorizontalSplit, VerticalSplit, CustomSplit, CustomCircular }
+
     internal class LookDevWindow : EditorWindow
     {
-        void OnEnable()
+        VisualElement views;
+        VisualElement environment;
+        
+        const string oneViewClass = "oneView";
+        const string twoViewsClass = "twoViews";
+        
+        LookDevContext.Display layout
         {
-            titleContent = LookDevStyle.WindowTitleAndIcon;
+            get => LookDev.currentContext.displayLayout;
+            set
+            {
+                if (LookDev.currentContext.displayLayout != value)
+                {
+                    if (value == LookDevContext.Display.HorizontalSplit || value == LookDevContext.Display.VerticalSplit)
+                    {
+                        if (views.ClassListContains(oneViewClass))
+                        {
+                            views.RemoveFromClassList(oneViewClass);
+                            views.AddToClassList(twoViewsClass);
+                        }
+                    }
+                    else
+                    {
+                        if (views.ClassListContains(twoViewsClass))
+                        {
+                            views.RemoveFromClassList(twoViewsClass);
+                            views.AddToClassList(oneViewClass);
+                        }
+                    }
+
+                    if (views.ClassListContains(LookDev.currentContext.displayLayout.ToString()))
+                        views.RemoveFromClassList(LookDev.currentContext.displayLayout.ToString());
+                    views.AddToClassList(value.ToString());
+
+                    var tmpContext = LookDev.currentContext;
+                    tmpContext.displayLayout = value;
+                    LookDev.currentContext = tmpContext;
+                }
+            }
         }
 
-        private void OnGUI()
+
+
+        void OnEnable()
         {
-            BeginWindows();
-            EndWindows();
+            //titleContent = LookDevStyle.WindowTitleAndIcon;
+            
+            rootVisualElement.styleSheets.Add(AssetDatabase.LoadAssetAtPath<StyleSheet>(LookDevStyle.k_uss));
+
+            views = new VisualElement() { name = "viewContainers" };
+            views.AddToClassList(layout == LookDevContext.Display.HorizontalSplit || layout == LookDevContext.Display.VerticalSplit ? "twoViews" : "oneView");
+            views.AddToClassList("container");
+            rootVisualElement.Add(views);
+            var viewA = new VisualElement() { name = "viewA" };
+            views.Add(viewA);
+            views.Add(new VisualElement() { name = "viewB" });
+
+            rootVisualElement.Add(new Button(() =>
+            {
+                if (layout  == LookDevContext.Display.HorizontalSplit)
+                    layout = LookDevContext.Display.FullA;
+                else if (layout == LookDevContext.Display.FullA)
+                    layout = LookDevContext.Display.HorizontalSplit;
+            }) { text = "One/Two views" });
         }
     }
 }

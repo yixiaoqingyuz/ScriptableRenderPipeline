6cf60c2372106a120bf9f7ade951c3e246c95bd6
RSlysz <remi@unity3d.com>
2019-03-11 13:13:19 +0100

    Add LookDev windows icon

--- com.unity.render-pipelines.core/Editor/LookDev/LookDevStyle.cs
+++ com.unity.render-pipelines.core/Editor/LookDev/LookDevStyle.cs
@@ -1,9 +1,12 @@
+using System;
 using UnityEngine;
 
 namespace UnityEditor.Rendering.LookDev
 {
     internal class LookDevStyle
     {
-        public static readonly GUIContent WindowTitleAndIcon = EditorGUIUtility.TrTextContentWithIcon("Look Dev", "UnityEditor.LookDevView");
+        const string k_IconFolder = @"Packages/com.unity.render-pipelines.core/Editor/LookDev/Icons/";
+        
+        public static readonly GUIContent WindowTitleAndIcon = EditorGUIUtility.TrTextContentWithIcon("Look Dev", CoreEditorUtils.LoadIcon(k_IconFolder, "LookDevMainIcon"));
     }
 }

9fe5557fccd04d909045d60f5644e25107c592b2
RSlysz <remi@unity3d.com>
2019-03-11 18:20:49 +0100

    Add view context infos

--- com.unity.render-pipelines.core/Editor/LookDev/LookDevContext.cs
+++ com.unity.render-pipelines.core/Editor/LookDev/LookDevContext.cs
@@ -1,6 +1,39 @@
+using UnityEngine;
+
 namespace UnityEditor.Rendering.LookDev
 {
-    internal struct LookDevContext
+    public struct LookDevContext
     {
+        public enum View { A, B };
+        public enum Display { FullA, FullB, HorizontalSplit, VerticalSplit, CustomSplit, CustomCircular }
+
+        public struct ViewContext
+        {
+            public readonly View view;
+            public ViewContext(View view) => this.view = view;
+
+            //[TODO: add object]
+            //[TODO: add object position]
+            //[TODO: add camera frustum]
+            //[TODO: add HDRI]
+        }
+
+        public static LookDevContext @default = new LookDevContext
+        {
+            viewA = new ViewContext(View.A),
+            viewB = new ViewContext(View.B)
+        };
+
+        public Display displayLayout;
+
+        public ViewContext viewA { get; private set; }
+        public ViewContext viewB { get; private set; }
+
+        //[TODO: add tool position ?]
+    }
+
+    public class LookDevSave : ScriptableObject
+    {
+        public LookDevContext context;
     }
 }

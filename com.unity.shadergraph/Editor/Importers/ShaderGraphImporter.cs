ff31b4536e0918c7d2c3240db59884b157eee3b2
Peter Bay Bastian <peter.bastian@gmail.com>
2019-03-14 13:26:08 +0100

    Fixed test failures, and added more details to Sub Graph error messages A merge accidentally made Shader Graphs import before Sub Graphs, which were then causing failures in tests using Sub Graphs.

--- com.unity.shadergraph/Editor/Importers/ShaderGraphImporter.cs
+++ com.unity.shadergraph/Editor/Importers/ShaderGraphImporter.cs
@@ -9,7 +9,7 @@ using UnityEditor.Graphing.Util;
 
 namespace UnityEditor.ShaderGraph
 {
-    [ScriptedImporter(24, Extension, 1)]
+    [ScriptedImporter(24, Extension, 3)]
     class ShaderGraphImporter : ScriptedImporter
     {
         public const string Extension = "shadergraph";

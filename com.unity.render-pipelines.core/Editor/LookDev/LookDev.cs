b7ae2226d1433941511e476708e473dd3cb1fff9
RSlysz <remi@unity3d.com>
2019-03-21 11:11:38 +0100

    Fix serialisation on reload and renderer usage

--- com.unity.render-pipelines.core/Editor/LookDev/LookDev.cs
+++ com.unity.render-pipelines.core/Editor/LookDev/LookDev.cs
@@ -3,59 +3,98 @@ using UnityEngine.Rendering.LookDev;
 
 using UnityEditor.UIElements;
 using UnityEditorInternal;
+using UnityEngine;
 
 namespace UnityEditor.Rendering.LookDev
 {
     /// <summary>
     /// Main entry point for scripting LookDev
     /// </summary>
-    public class LookDev
+    public static class LookDev
     {
-        static ILookDevDataProvider dataProvider => RenderPipelineManager.currentPipeline as ILookDevDataProvider;
+        const string lastRenderingDataSavePath = "Library/LookDevConfig.asset";
+
+        static LookDevWindow window;
+        static LookDevRenderer renderer;
+        
+        public static bool open { get; private set; }
+
+        static ILookDevDataProvider dataProvider
+            => RenderPipelineManager.currentPipeline as ILookDevDataProvider;
 
         /// <summary>
         /// Does LookDev is supported with the current render pipeline?
         /// </summary>
         public static bool supported => dataProvider != null;
         
+        public static LookDevContent sceneContents { get; set; } = new LookDevContent();
+        
+        public static LookDevContext currentContext { get; set; }
+
+        static LookDev()
+            => currentContext = LoadConfigInternal() ?? GetDefaultContext();
+
+        static LookDevContext GetDefaultContext()
+            => UnityEngine.ScriptableObject.CreateInstance<LookDevContext>();
+
+        public static void ResetConfig()
+            => currentContext = GetDefaultContext();
+
+        static LookDevContext LoadConfigInternal(string path = lastRenderingDataSavePath)
+        {
+            var last = InternalEditorUtility.LoadSerializedFileAndForget(path)?[0] as LookDevContext;
+            if (last != null && !last.Equals(null))
+                return ((LookDevContext)last);
+            return null;
+        }
+
+        public static void LoadConfig(string path = lastRenderingDataSavePath)
+        {
+            var last = LoadConfigInternal(path);
+            if (last != null)
+                currentContext = last;
+        }
+
+        public static void SaveConfig(string path = lastRenderingDataSavePath)
+            => InternalEditorUtility.SaveToSerializedFileAndForget(new[] { currentContext }, path, true);
+
         [MenuItem("Window/Experimental/NEW Look Dev", false, -1)]
-        static void ShowLookDevTool()
+        public static void Open()
         {
-            LoadConfig();
-
-            LookDevWindow window = EditorWindow.GetWindow<LookDevWindow>();
+            window = EditorWindow.GetWindow<LookDevWindow>();
             window.titleContent = LookDevStyle.WindowTitleAndIcon;
+            ConfigureLookDev();
         }
 
-        const string lastContextSavePath = "Library/LookDevConfig.asset";
-
-        static LookDevContext s_currentContext = LookDevContext.@default;
-
-        public static LookDevContext currentContext
+        [Callbacks.DidReloadScripts]
+        static void OnEditorReload()
         {
-            get => s_currentContext;
-            set
+            var windows = Resources.FindObjectsOfTypeAll<LookDevWindow>();
+            window = windows.Length > 0 ? windows[0] : null;
+            open = window != null;
+            if (open)
+                ConfigureLookDev();
+        }
+
+        static void ConfigureLookDev()
+        {
+            open = true;
+            LoadConfig();
+            ConfigureRenderer();
+        }
+
+        static void ConfigureRenderer()
+        {
+            renderer = new LookDevRenderer(window, currentContext, sceneContents);
+            window.OnWindowClosed += () =>
             {
-                s_currentContext = value;
+                renderer.CleanUp();
+                renderer = null;
+
                 SaveConfig();
-            }
+
+                open = false;
+            };
         }
-
-        public static void ResetConfig() => s_currentContext = LookDevContext.@default;
-
-        public static void LoadConfig(string path = lastContextSavePath)
-        {
-            var last = InternalEditorUtility.LoadSerializedFileAndForget(path)?[0];
-            if (last != null && !last.Equals(null) && last is LookDevSave)
-                s_currentContext = ((LookDevSave)last).context;
-        }
-
-        public static void SaveConfig(string path = lastContextSavePath)
-        {
-            var save = UnityEngine.ScriptableObject.CreateInstance<LookDevSave>();
-            save.context = s_currentContext;
-            InternalEditorUtility.SaveToSerializedFileAndForget(new[] { save }, path, true);
-        }
-
     }
 }
